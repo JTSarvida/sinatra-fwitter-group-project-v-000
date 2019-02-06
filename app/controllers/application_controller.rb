eb506768a529c426417a3a760f5f250b22a9e3d8
Joshua Tan <jtsarvida@gmail.com>
2019-02-06 19:23:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,18 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :index
+  end
 
+  helpers do
+    def current_user
+      @user = User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
 end
